45857bb01af959d3a84d66849cda20dc2a94d22e
Shaffat Dipu <msdipu12@gmail.com>
2020-12-10 14:30:19 +0600

    build sample proect from documentation

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from .models import Question,Choice
 
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -15,12 +16,17 @@ class IndexView(generic.ListView):
         '''
         Return the last five pubished questions.
         '''
-        return Question.objects.order_by('-pub_date')[:5]
-    
+        return Question.objects.filter(pub_date__lte=timezone.now() ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        '''
+        Exclude any questions that arent published yet
+        '''
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.ListView):
